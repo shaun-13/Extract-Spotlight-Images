783452e12d8d4b689d03bacf93dbb6ee135837c6
Shaun Phua <spjh1997@hotmail.com>
2022-12-07 01:58:55 +0800

    Create portrait_cleanup.py
    This function deletes any portrait images in the destination folders

---
+++ portrait_cleanup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import cv2
+
+dst = os.path.join(os.environ['USERPROFILE'], 'Pictures/Spotlight Images/')
+
+def portrait_cleanup(dst):
+    print("Portrait Spotlight Images Cleanup is running...\n")
+    for filename in os.listdir(dst):
+        if ".jpg" not in filename:
+            continue
+        dst_filename = dst + filename
+        im = cv2.imread(dst_filename)
+        # h is the image height, and w is the width. Note that for portrait images w is always less that h
+        h, w, c = im.shape
+        if w < h:
+            print("File removed: " + filename + " \nHeight: " + str(h) + " \nWidth: " + str(w) + "\n" )
+            os.remove(dst_filename) 
+
+if __name__ == "__main__":
+    portrait_cleanup(dst)
